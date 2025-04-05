96dd761f80074b9cb94bdf6f624f4b1ec5a2c5b4
Holger Grosse-Plankermann <holgergp@gmail.com>
2025-04-05 11:11:59 +0200

    refactor(timer mocks): remove unused util

--- src/utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-export const debounce = (fn: Function, ms = 300) => {
-  let timeoutId: ReturnType<typeof setTimeout>;
-  console.log('debounce')
-  return function (this: unknown, ...args: unknown[]) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => fn.apply(this, args), ms);
-  };
-};
